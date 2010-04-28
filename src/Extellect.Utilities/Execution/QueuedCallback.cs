9896f57925b82a4514a7d1ad0a5e26bb7d132900
jono.pare <jono.pare@1d36d2e2-0b34-11de-97d8-e990b0f60ea0>
2010-04-28 22:43:46 +0000

    added basic queued execution functions. they're not as good or sophisticated as Microsoft's CCR, but they are useful nonetheless when you need to run a series of commands in parallel. The real joy will be in the future callback (when an asynchronous method finishes executing, it will have the opportunity to queue more commands)

---
+++ src/Extellect.Utilities/Execution/QueuedCallback.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Extellect.Utilities.Collections;
+
+namespace Extellect.Utilities.Execution
+{
+    /// <summary>
+    /// This delegate accepts some kind of state, as well as a queue onto which further
+    /// callbacks can be queued, via the queued callback command construct.
+    /// </summary>
+    public delegate void QueuedCallback(object state, BlockingQueue<QueuedCallbackCommand> queue);
+}
