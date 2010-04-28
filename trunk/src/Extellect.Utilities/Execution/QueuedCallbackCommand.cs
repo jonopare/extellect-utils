05071945b1b7b6b9e049de75499e10181f36bb3f
jono.pare <jono.pare@1d36d2e2-0b34-11de-97d8-e990b0f60ea0>
2010-04-28 22:43:46 +0000

    added basic queued execution functions. they're not as good or sophisticated as Microsoft's CCR, but they are useful nonetheless when you need to run a series of commands in parallel. The real joy will be in the future callback (when an asynchronous method finishes executing, it will have the opportunity to queue more commands)

---
+++ trunk/src/Extellect.Utilities/Execution/QueuedCallbackCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Extellect.Utilities.Collections;
+
+namespace Extellect.Utilities.Execution
+{
+    /// <summary>
+    /// An action that will be executed later.
+    /// </summary>
+    public class QueuedCallbackCommand
+    {
+        private readonly BlockingQueue<QueuedCallbackCommand> queue;
+        private readonly QueuedCallback callback;
+        private readonly object state;
+
+        /// <summary>
+        /// Constructs a new queued callback command. The parameters represent the method to call, the state
+        /// to pass into that method call, and a queue so that additional commands can be built up
+        /// for future execution.
+        /// </summary>
+        public QueuedCallbackCommand(QueuedCallback callback, object state, BlockingQueue<QueuedCallbackCommand> queue)
+        {
+            this.callback = callback;
+            this.state = state;
+            this.queue = queue;
+        }
+
+        /// <summary>
+        /// Invokes this command object's callback delegate, passing in any state and a reference to a queue.
+        /// </summary>
+        public void Execute()
+        {
+            callback(state, queue);
+        }
+    }
+}
