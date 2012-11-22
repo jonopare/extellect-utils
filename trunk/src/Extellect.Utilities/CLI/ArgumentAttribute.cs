daeffa95c560e63787da2a42f85c5c5c4c95234e
jono.pare@gmail.com <jono.pare@gmail.com@1d36d2e2-0b34-11de-97d8-e990b0f60ea0>
2012-11-22 14:24:01 +0000

    Added utilities for command line argument parsing

---
+++ trunk/src/Extellect.Utilities/CLI/ArgumentAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Extellect.Utilities.CLI
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class ArgumentAttribute : Attribute
+    {
+        public string Name { get; set; }
+        public string Default { get; set; }
+        public bool Required { get; set; }
+    }
+}
