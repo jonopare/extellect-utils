5a0874ed09ce303d5570130ea9e4c215ccadad49
jono.pare@gmail.com <jono.pare@gmail.com@1d36d2e2-0b34-11de-97d8-e990b0f60ea0>
2013-06-09 09:45:25 +0000

    added "indexable" classes, allowing quick lookup of items by multiple keys

---
+++ src/Extellect.Utilities.Tests.Unit/Collections/IndexableTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Extellect.Utilities.Collections;
+
+namespace Extellect.Utilities.Tests.Unit.Collections
+{
+    [TestClass]
+    public class IndexableTests
+    {
+        public class TestItem
+        {
+            public int Id;
+            public string Code;
+            public string Description;
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Indexable<TestItem> items = new Indexable<TestItem>();
+            var byId = items.CreateUniqueIndex<int>(i => i.Id);
+            var byCode = items.CreateUniqueIndex<string>(i => i.Code);
+            items.Add(new TestItem { Id = 1, Code = "One", Description = "First" });
+            items.Add(new TestItem { Id = 2, Code = "Two", Description = "Second" });
+            Console.WriteLine(byCode["One"].Description);
+        }
+    }
+}
