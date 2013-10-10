7d2171e9112bc03979c3505432c4e6a1bd7cbb7f
jono.pare@gmail.com <jono.pare@gmail.com@1d36d2e2-0b34-11de-97d8-e990b0f60ea0>
2013-10-10 19:17:00 +0000

    added another csv reader but this one has a more IDataReader style approach, yielding control after each row. clearly the extra function calls translate to a performance overhead in a very large csv file.

---
+++ src/Extellect.Utilities/Data/CsvReader.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Extellect.Utilities.Data
+{
+    /// <summary>
+    /// Encapsulates the logic required to read comma separated values in a forward-only direction.
+    /// Subclasses add behaviour that will be invoked after each field or new line, and could also
+    /// add their own buffers to allow random access to fields within a row.
+    /// </summary>
+    public abstract class CsvReader : IDisposable
+    {
+        private const char Separator = ',';
+        private const char Quote = '"';
+        private const char CarriageReturn = '\r';
+        private const char NewLine = '\n';
+
+        private char[] inbuf;
+        private int inbufcount;
+        private int inbufpos;
+
+        private char[] outbuf;
+        private int outbufcount;
+
+        private TextReader csv;
+
+        /// <summary>
+        /// Gets the string value of the current field.
+        /// </summary>
+        protected string CurrentField
+        {
+            get
+            {
+                return new string(outbuf, 0, outbufcount);
+            }
+        }
+
+        /// <summary>
+        /// Gets the zero-based current row index of the reader.
+        /// </summary>
+        protected int Row { get; private set; }
+
+        /// <summary>
+        /// Gets the zero-based current column index of the reader.
+        /// </summary>
+        protected int Column { get; private set; }
+
+        /// <summary>
+        /// Allows derived classes to provide behaviour that will be invoked at the end of each field.
+        /// </summary>
+        protected abstract void DoEndOfField();
+
+        /// <summary>
+        /// Allows derived classes to provide behaviour that will be invoked at the end of each line.
+        /// </summary>
+        //protected abstract void DoEndOfLine();
+
+        /// <summary>
+        /// Creates a new Csv object with the default maximum single output field capacity.
+        /// </summary>
+        public CsvReader(TextReader csv)
+            : this(csv, 8 * 1024)
+        {
+        }
+
+        /// <summary>
+        /// Creates a new Csv object.
+        /// </summary>
+        /// <param name="fieldCapacity">The maximum size of a single output field in the CSV.</param>
+        public CsvReader(TextReader csv, int fieldCapacity)
+        {
+            this.csv = csv;
+            this.outbuf = new char[fieldCapacity];
+
+            this.inbuf = new char[8 * 1024];
+            this.inbufcount = -1;
+            this.inbufpos = 0;
+
+            this.Row = -1;
+        }
+
+        /// <summary>
+        /// Reads the specified CSV stream. The abstract methods DoEndOfField and DoEndOfLine
+        /// are invoked at the appropriate points in the stream. You should override these two 
+        /// methods in a derived class to make use of the CSV reader.
+        /// </summary>
+        /// <remarks>
+        /// Implementation of CSV state machine that uses aggressive inlining and jumps between
+        /// goto statements for performance reasons.
+        /// </remarks>
+        /// <param name="csv">A TextReader that contains CSV formatted data.</param>
+        public bool Read()
+        {
+            Column = 0;
+            Row++;
+            outbufcount = 0;
+            char c;
+        Separator:
+            if (inbufpos >= inbufcount)
+            {
+                inbufcount = csv.Read(inbuf, 0, inbuf.Length);
+                if (inbufcount == 0)
+                {
+                    DoEndOfField();
+                    outbufcount = 0;
+                    return false;
+                }
+                inbufpos = 0;
+            }
+            c = inbuf[inbufpos++];
+            switch (c)
+            {
+                case CsvReader.Separator:
+                    DoEndOfField();
+                    outbufcount = 0;
+                    Column++;
+                    goto Separator;
+                case CsvReader.Quote:
+                    goto QuotedField;
+                case CsvReader.NewLine:
+                    DoEndOfField();
+                    outbufcount = 0;
+                    return true;
+                    //goto Separator;
+                case CsvReader.CarriageReturn:
+                    goto CarriageReturn;
+                default:
+                    outbuf[outbufcount++] = c;
+                    goto Field;
+            }
+        Field:
+            if (inbufpos >= inbufcount)
+            {
+                inbufcount = csv.Read(inbuf, 0, inbuf.Length);
+                if (inbufcount == 0)
+                {
+                    DoEndOfField();
+                    outbufcount = 0;
+                    return false;
+                }
+                inbufpos = 0;
+            }
+            c = inbuf[inbufpos++];
+            switch (c)
+            {
+                case CsvReader.Separator:
+                    DoEndOfField();
+                    outbufcount = 0;
+                    Column++;
+                    goto Separator;
+                case CsvReader.NewLine:
+                    DoEndOfField();
+                    outbufcount = 0;
+                    goto Separator;
+                case CsvReader.CarriageReturn:
+                    goto CarriageReturn;
+                default:
+                    outbuf[outbufcount++] = c;
+                    goto Field;
+            }
+        QuotedField:
+            if (inbufpos >= inbufcount)
+            {
+                inbufcount = csv.Read(inbuf, 0, inbuf.Length);
+                if (inbufcount == 0)
+                {
+                    throw new FormatException("Found EOF inside quoted field");
+                }
+                inbufpos = 0;
+            }
+            c = inbuf[inbufpos++];
+            switch (c)
+            {
+                case CsvReader.Quote:
+                    goto QuoteInQuotedField;
+                default:
+                    outbuf[outbufcount++] = c;
+                    goto QuotedField;
+            }
+        QuoteInQuotedField:
+            if (inbufpos >= inbufcount)
+            {
+                inbufcount = csv.Read(inbuf, 0, inbuf.Length);
+                if (inbufcount == 0)
+                {
+                    DoEndOfField();
+                    outbufcount = 0;
+                    return false;
+                }
+                inbufpos = 0;
+            }
+            c = inbuf[inbufpos++];
+            switch (c)
+            {
+                case CsvReader.Quote:
+                    outbuf[outbufcount++] = c;
+                    goto QuotedField;
+                case CsvReader.Separator:
+                    DoEndOfField();
+                    outbufcount = 0;
+                    Column++;
+                    goto Separator;
+                case CsvReader.NewLine:
+                    DoEndOfField();
+                    outbufcount = 0;
+                    return true;
+                    //goto Separator;
+                case CsvReader.CarriageReturn:
+                    goto CarriageReturn;
+                default:
+                    throw new FormatException(string.Format("Found unexpected character '\\u{0:x4}' after quote in quoted field at position {1}. Only allowed characters are another quote, comma, EOL or EOF.", c, Position));
+            }
+        CarriageReturn:
+            if (inbufpos >= inbufcount)
+            {
+                inbufcount = csv.Read(inbuf, 0, inbuf.Length);
+                if (inbufcount == 0)
+                {
+                    throw new FormatException("Found EOF after carriage return.");
+                }
+                inbufpos = 0;
+            }
+            c = inbuf[inbufpos++];
+            if (c != CsvReader.NewLine)
+            {
+                throw new FormatException(string.Format("Found unexpected character '\\u{0:x4}' after carriage return at position {1}. Only allowed character is line feed.", c, Position));
+            }
+            DoEndOfField();
+            outbufcount = 0;
+            return true;
+            //goto Separator;
+        }
+
+        private string Position
+        {
+            get
+            {
+                return "R" + Row + "C" + Column;
+            }
+        }
+
+        public void Dispose()
+        {
+            if (csv != null)
+            {
+                csv.Dispose();
+                csv = null;
+            }
+            if (inbuf != null)
+            {
+                inbuf = null;
+            }
+            if (outbuf != null)
+            {
+                outbuf = null;
+            }
+        }
+    }
+}
+
