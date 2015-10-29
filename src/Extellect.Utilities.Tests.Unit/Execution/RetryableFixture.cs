e1d1120ae3dc04a6d3d96b3040a4d01ec5789d57
jonopare <jono.pare+github@gmail.com>
2015-10-29 08:54:20 +0200

    added Retryable
    Retryable encapsulates simple iterative retry behaviour with a maximum
    retry count, an action to perform when a retry occurs, and a collection
    of predicates that identify a retryable exception

---
+++ src/Extellect.Utilities.Tests.Unit/Execution/RetryableFixture.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Extellect.Utilities.Execution
+{
+    [TestClass]
+    public class RetryableFixture
+    {
+        [TestMethod]
+        public void Retry_ThrowsTwiceSucceedsThirdAttempt()
+        {
+            var retried = new Dictionary<int, Exception>();
+            var n = 0;
+            string m = null;
+
+            Retryable.Retry(() => { if (n++ < 2) { throw new Exception("No"); } else { m = "Yes"; } }, 3, (e, i) => { retried.Add(i, e); }, e => e.Message == "No");
+
+            Assert.AreEqual(3, n);
+            Assert.AreEqual(2, retried.Count);
+            Assert.AreEqual("Yes", m);
+        }
+    }
+}
---
+++ src/Extellect.Utilities/Execution/Retryable.cs
@@ -0,0 +1,45 @@
+﻿#pragma warning disable 1591
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Extellect.Utilities.Execution
+{
+    public class Retryable
+    {
+        public static void Retry(Action action, int maxRetries, Action<Exception, int> onRetry, params Func<Exception, bool>[] canRetryPredicates)
+        {
+            var retries = 0;
+            do
+            {
+                try
+                {
+                    action();
+                    break;
+                }
+                catch (Exception e)
+                {
+                    if (retries++ < maxRetries)
+                    {
+                        var canRetry = false;
+                        foreach (var canRetryPredicate in canRetryPredicates)
+                        {
+                            if (canRetryPredicate(e))
+                            {
+                                canRetry = true;
+                                onRetry(e, retries - 1);
+                                break;
+                            }
+                        }
+                        if (canRetry)
+                        {
+                            continue;
+                        }
+                    }
+                    throw;
+                }
+            } while (true);
+        }
+    }
+}
