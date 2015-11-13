dd4035195394df0f573cecb21c818357cba4bd7f
jonopare <jono.pare+github@gmail.com>
2015-11-13 16:48:53 +0200

    Added first draft of MapReduce
    Added first draft of MapReduce

---
+++ src/Extellect.Utilities.Tests.Unit/Execution/MapReduceFixture.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Extellect.Utilities.Execution
+{
+    [TestClass]
+    public class MapReduceFixture
+    {
+        private class LetterCount : MapReduce<string, char, int>
+        {
+            protected override IEnumerable<KeyValuePair<char, int>> Map(string input)
+            {
+                return input.Select(x => new KeyValuePair<char, int>(x, 1));
+            }
+
+            protected override KeyValuePair<char, int> Reduce(KeyValuePair<char, IEnumerable<int>> intermediate)
+            {
+                return new KeyValuePair<char, int>(intermediate.Key, intermediate.Value.Sum());
+            }
+        }
+
+        private readonly string[] _letterCountInput = new[] { "the", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog" };
+
+        private readonly Dictionary<char, int> _expected = new Dictionary<char, int>()
+            {
+                { 'a', 1 },
+                { 'b', 1 },
+                { 'c', 1 },
+                { 'd', 1 },
+                { 'e', 3 },
+                { 'f', 1 },
+                { 'g', 1 },
+                { 'h', 2 },
+                { 'i', 1 },
+                { 'j', 1 },
+                { 'k', 1 },
+                { 'l', 1 },
+                { 'm', 1 },
+                { 'n', 1 },
+                { 'o', 4 },
+                { 'p', 1 },
+                { 'q', 1 },
+                { 'r', 2 },
+                { 's', 1 },
+                { 't', 2 },
+                { 'u', 2 },
+                { 'v', 1 },
+                { 'w', 1 },
+                { 'x', 1 },
+                { 'y', 1 },
+                { 'z', 1 },
+            };
+
+        [TestMethod]
+        public void SanityCheck()
+        {
+            Dictionary<char, int> actual = Enumerable.Range('a', 26).ToDictionary(x => (char)x, x => 0);
+            foreach (var letter in _letterCountInput.SelectMany(x => x))
+            {
+                actual[letter]++;
+            }
+
+            AssertionHelper.AreSequenceEqual(_expected.OrderBy(x => x.Key), actual.OrderBy(x => x.Key));
+        }
+
+        [TestMethod]
+        public void MapReduce_Run_LetterCount()
+        {
+            var actual = new Dictionary<char, int>();
+
+            var letterCount = new LetterCount();
+            letterCount.Run(
+                _letterCountInput,
+                x => actual.Add(x.Key, x.Value)
+                ).Wait();
+
+            AssertionHelper.AreSequenceEqual(_expected.OrderBy(x => x.Key), actual.OrderBy(x => x.Key));
+        }
+    }
+}
---
+++ src/Extellect.Utilities/Execution/MapReduce.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using System.Threading.Tasks.Dataflow;
+
+namespace Extellect.Utilities.Execution
+{
+    /// <summary>
+    /// A silly little class to enable proof-of-concept work with the MapReduce pattern.
+    /// Override the Map and Reduce methods, then invoke Run with inputs.
+    /// I tried a version that used Dataflow but it was built with .NET 4.5 and doesn't 
+    /// work so great with .NET 4.0
+    /// </summary>
+    public abstract class MapReduce<TInput, TKey, TOutput>
+    {
+        /// <summary>
+        /// Map is called once for each input, and returns zero-to-many key value pairs
+        /// that go into the ShuffleSort phase.
+        /// </summary>
+        protected abstract IEnumerable<KeyValuePair<TKey, TOutput>> Map(TInput input);
+
+        /// <summary>
+        /// After the ShuffleSort phase is complete, the Reduce function will be called
+        /// once for each Key, with zero-to-many Output values associated with that key.
+        /// </summary>
+        protected abstract KeyValuePair<TKey, TOutput> Reduce(KeyValuePair<TKey, IEnumerable<TOutput>> intermediate);
+
+        /// <summary>
+        /// TODO: rewrite it to be asynchronous without using C# 5.0
+        /// </summary>
+        public Task Run(IEnumerable<TInput> inputs, Action<KeyValuePair<TKey, TOutput>> result)
+        {
+            var mapPhase = new TaskCompletionSource<bool>();
+
+            var intermediates = new Dictionary<TKey, List<TOutput>>();
+            foreach (var input in inputs)
+            {
+                foreach (var intermediate in Map(input))
+                {
+                    List<TOutput> values;
+                    if (!intermediates.TryGetValue(intermediate.Key, out values))
+                    {
+                        intermediates.Add(intermediate.Key, values = new List<TOutput>());
+                    }
+                    values.Add(intermediate.Value);
+                }
+            }
+
+            mapPhase.SetResult(true);
+
+            return mapPhase.Task.ContinueWith(_ =>
+            {
+                var reducePhase = new TaskCompletionSource<bool>();
+
+                foreach (var intermediate in intermediates)
+                {
+                    result(Reduce(new KeyValuePair<TKey, IEnumerable<TOutput>>(intermediate.Key, intermediate.Value)));
+                }
+
+                reducePhase.SetResult(true);
+
+                return reducePhase.Task;
+            });
+
+            //throw new NotImplementedException();
+            //var map = new TransformBlock<TInput, IEnumerable<KeyValuePair<TKey, TOutput>>>(new Func<TInput, IEnumerable<KeyValuePair<TKey, TOutput>>>(Map));
+
+            //var intermediates = new Dictionary<TKey, IEnumerable<TOutput>>();
+            //var shuffle = new ActionBlock<IEnumerable<KeyValuePair<TKey, TOutput>>>(x =>
+            //{
+            //    foreach (var intermediate in x)
+            //    {
+            //        IEnumerable<TOutput> values;
+            //        if (!intermediates.TryGetValue(intermediate.Key, out values))
+            //        {
+            //            intermediates.Add(intermediate.Key, values = new List<TOutput>());
+            //        }
+            //        ((List<TOutput>)values).Add(intermediate.Value);
+            //    }
+            //});
+
+            //map.LinkTo(shuffle, new DataflowLinkOptions { PropagateCompletion = true });
+
+            //foreach (var input in inputs)
+            //{
+            //    map.Post(input);
+            //}
+            //map.Complete();
+
+            //shuffle.Completion.Wait();
+
+            //var reduce = new TransformBlock<KeyValuePair<TKey, IEnumerable<TOutput>>, KeyValuePair<TKey, TOutput>>(new Func<KeyValuePair<TKey, IEnumerable<TOutput>>, KeyValuePair<TKey, TOutput>>(Reduce));
+            //var notifyResult = new ActionBlock<KeyValuePair<TKey, TOutput>>(new Action<KeyValuePair<TKey,TOutput>>(result));
+
+            //reduce.LinkTo(notifyResult, new DataflowLinkOptions { PropagateCompletion = true });
+
+            //foreach (var intermediate in intermediates)
+            //{
+            //    reduce.Post(intermediate);
+            //}
+            //reduce.Complete();
+
+            //return notifyResult.Completion;
+        }
+    }
+}
