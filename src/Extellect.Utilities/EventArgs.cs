e6aa37a2dcba7906e7440d4299db7967696fcb5d
jonopare <jono.pare+github@gmail.com>
2016-11-13 16:24:40 +0200

    Added generic EventArgs
    Added generic EventArgs with immutable value of type T

---
+++ src/Extellect.Utilities/EventArgs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Extellect.Utilities
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class EventArgs<T> : EventArgs
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public T Item { get; private set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public EventArgs(T item)
+        {
+            Item = item;
+        }
+    }
+}
