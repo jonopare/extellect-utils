dbbe791d32818312bc15ecfd259669afc4a73855
jonopare <jono.pare+github@gmail.com>
2017-03-07 18:18:44 +0200

    Dictionary extensions and Cache Entries
    Dictionary extensions and Cache Entries

--- src/SolutionVersionInfo.cs
+++ src/SolutionVersionInfo.cs
@@ -3,3 +3,5 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
+[assembly: AssemblyVersion("1.1.5.0")]
+[assembly: AssemblyFileVersion("1.1.5.0")]
