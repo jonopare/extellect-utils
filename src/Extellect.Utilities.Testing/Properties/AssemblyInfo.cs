cb6407e5abcdbe0a6017dfeb5f6701ca8edde832
jonopare <jono.pare+github@gmail.com>
2018-03-10 15:29:55 +0200

    Version update
    Version update

--- src/Extellect.Utilities.Testing/Properties/AssemblyInfo.cs
+++ src/Extellect.Utilities.Testing/Properties/AssemblyInfo.cs
@@ -8,29 +8,3 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Extellect.Utilities.Testing")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Extellect.Utilities.Testing")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a085aa00-11c2-462c-b7ea-26447b15e3a6")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
