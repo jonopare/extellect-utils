fb63d4ff936ace4be521ebe0fa4da6889f5e94da
jonopare <jono.pare+github@gmail.com>
2018-03-11 20:33:54 +0200

    Moving things around :(
    Badly named things got moved. Extracted Reactive code (one class)

---
+++ src/Extellect.Utilities.UnitTests/Collections/IndexableTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using Extellect.Utilities.Collections;
+
+namespace Extellect.Utilities.Collections
+{
+    
+    public class IndexableTests
+    {
+        public class TestItem
+        {
+            public int Id;
+            public string Code;
+            public string Description;
+        }
+
+        [Fact]
+        public void CreateUniqueIndex_DistinctValues_AllowsRetrieval()
+        {
+            Indexable<TestItem> items = new Indexable<TestItem>();
+            var byId = items.CreateUniqueIndex<int>(i => i.Id);
+            var byCode = items.CreateUniqueIndex<string>(i => i.Code);
+            items.Add(new TestItem { Id = 1, Code = "One", Description = "First" });
+            items.Add(new TestItem { Id = 2, Code = "Two", Description = "Second" });
+            Assert.Equal("One", byId[1].Code);
+            Assert.Equal("Two", byId[2].Code);
+        }
+
+        [Fact]
+        public void CreateUniqueIndex_NonDistinctValues_FailsAtInsertion()
+        {
+            Indexable<TestItem> items = new Indexable<TestItem>();
+            var byId = items.CreateUniqueIndex<int>(i => i.Id);
+            var byCode = items.CreateUniqueIndex<string>(i => i.Code);
+            items.Add(new TestItem { Id = 1, Code = "One", Description = "First" });
+            Assert.Throws<ArgumentException>(() => items.Add(new TestItem { Id = 2, Code = "One", Description = "Duplicate of first" }));
+            // ^^ should fail at above line with message: "An entry with the same key already exists."
+        }
+    }
+}
---
+++ src/Extellect.Utilities.UnitTests/Polling/BackOffTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Extellect.Utilities.Testing;
+
+namespace Extellect.Utilities.Polling
+{
+    
+    public class BackOffTests
+    {
+        [Fact]
+        public void Wait_WithDefaultWaitAction_ActuallyWaits()
+        {
+            TimeSpan expected = TimeSpan.FromMilliseconds(500);
+            BackOff backOff = new BackOff(expected, TimeSpan.MaxValue);
+
+            DateTime start = DateTime.Now;
+            backOff.Wait();
+            DateTime finish = DateTime.Now;
+            
+            Assert.True(finish - start > expected);
+        }
+
+        [Fact]
+        public void Wait_WithFakeDelay_Waits()
+        {
+            TimeSpan expectedWait = TimeSpan.Zero;
+            var fakeBlock = new TestBlock();
+
+            TimeSpan initialWait = TimeSpan.FromMilliseconds(250);
+            TimeSpan maxWait = TimeSpan.FromMilliseconds(550);
+            BackOff backOff = new BackOff(initialWait, maxWait, fakeBlock);
+
+            var expectedWaits = new List<TimeSpan>();
+
+            // wait once
+            expectedWaits.Add(initialWait);
+            backOff.Wait();
+
+            // wait a second time
+            expectedWaits.Add(initialWait + initialWait);
+            backOff.Wait();
+
+            // this wait would exceed the specified max wait, so only wait the max
+            expectedWaits.Add(maxWait);
+            backOff.Wait();
+
+            // wait the max again
+            expectedWaits.Add(maxWait);
+            backOff.Wait();
+
+            // reset to initial
+            backOff.Reset();
+
+            // repeat the above sequence
+            expectedWaits.Add(initialWait);
+            backOff.Wait();
+
+            expectedWaits.Add(initialWait + initialWait);
+            backOff.Wait();
+
+            expectedWaits.Add(maxWait);
+            backOff.Wait();
+
+            expectedWaits.Add(maxWait);
+            backOff.Wait();
+
+            AssertionHelper.AreSequencesEqual(fakeBlock.ActualWaits, expectedWaits);
+        }
+    }
+}
