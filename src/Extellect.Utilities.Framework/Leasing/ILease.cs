29ff8d067bc78dcc8fc9f2e22c5eeaba079c79cb
jonopare <jono.pare+github@gmail.com>
2019-02-17 17:43:59 +0200

    Much moving around to support .NET Standard 2.0

---
+++ src/Extellect.Utilities.Framework/Leasing/ILease.cs
@@ -0,0 +1,14 @@
+﻿#pragma warning disable 1591
+using System;
+
+namespace Extellect.Leasing
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface ILease
+    {
+        bool IsExpired { get; }
+        void Renew(TimeSpan amount);
+    }
+}
