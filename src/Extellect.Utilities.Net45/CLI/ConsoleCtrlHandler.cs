9402597b705227e1188b5c4bde723e7da40bc7c7
jonopare <jono.pare+github@gmail.com>
2019-02-17 15:23:40 +0200

    Am i losing code?

--- src/Extellect.Utilities.Net45/CLI/ConsoleCtrlHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Extellect.Utilities.CLI
-{
-    internal delegate bool ConsoleCtrlHandler(int value);
-}
