4c70fa22edeb2c18330fa4f0cf8e4b4cd2432aa5
Jono Pare <jono.pare@gmail.com>
2025-05-12 10:55:53 +0200

    Add a DateOnly variant of Int32.ToDate

--- src/Extellect.Core/Int32Extensions.cs
+++ src/Extellect.Core/Int32Extensions.cs
@@ -8,9 +8,14 @@
         /// <summary>
         /// 
         /// </summary>
-        public static DateTime ToDate(this int value)
+        public static DateTime ToDateTime(this int value)
         {
             return new DateTime(value / 10000, (value / 100) % 100, value % 100);
+        }
+
+        public static DateOnly ToDate(this int value)
+        {
+            return new DateOnly(value / 10000, (value / 100) % 100, value % 100);
         }
     }
 }
